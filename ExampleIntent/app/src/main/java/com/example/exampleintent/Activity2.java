61cd38c753aa953ab5c20b7327f9a1852e57426d
bhargavi <maruboyenabhargavicse.gist@gmail.com>
2021-06-01 18:09:20 +0530

    updated

---
+++ ExampleIntent/app/src/main/java/com/example/exampleintent/Activity2.java
@@ -0,0 +1,17 @@
+package com.example.exampleintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        String n = getIntent().getStringExtra("key");
+        Toast.makeText(this,"Welcome "+n,Toast.LENGTH_SHORT).show();
+    }
+}
